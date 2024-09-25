9b25f5e2334834431f06780a48416bccc33b0b10
IT24101937 <it24101937@my.sliit.lk>
2024-09-25 17:06:31 +0530

    Add files via upload

---
+++ Q2/It24101937Lab7Q2A.java
@@ -0,0 +1,18 @@
+public class It24101937Lab7Q2A{
+	public static void main(String[]args){
+		
+		for (int i = 1; i <= 4; i=i+1)
+		{
+			//System.out.print("$");
+            
+            for (int j = 1; j<=5; j=j+1)
+			{
+				System.out.print("$");
+			}
+            System.out.println();
+        }
+
+	
+
+	}
+}
